84ec9fced5db06a3d658404f03d51718c80970d0
nilaybesli <nil.besli@hotmail.com>
2024-04-21 18:42:35 +0300

    landing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/firebase_options.dart';
-import 'package:whatsapp_clone/futures/landing/screens/landing_screen.dart';
 
 import 'colors.dart';
+import 'features/landing/screens/landing_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
