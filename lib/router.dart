cd7be2040edf5f417ced6dd5721714793225c1fe
nilaybesli <nil.besli@hotmail.com>
2024-04-23 03:03:42 +0300

    firebase phone authentication

--- lib/router.dart
+++ lib/router.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/widgets/error.dart';
 import 'package:whatsapp_clone/features/auth/screens/login_screen.dart';
+import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case LoginScreen.routeName:
-      return MaterialPageRoute(builder: (ctx) => const LoginScreen(),
+      return MaterialPageRoute(
+        builder: (ctx) => const LoginScreen(),
+      );
+    case OTPScreen.routeName:
+      final verificationId = settings.arguments as String;
+      return MaterialPageRoute(
+        builder: (ctx) => OTPScreen(
+          verificationId: verificationId,
+        ),
       );
     default:
-      return MaterialPageRoute(builder: (context)=>const Scaffold(
-
-        body: ErrorScreen(error: 'This page does not exist'),
-
-      ));
+      return MaterialPageRoute(
+          builder: (context) => const Scaffold(
+                body: ErrorScreen(error: 'This page does not exist'),
+              ));
   }
 }
